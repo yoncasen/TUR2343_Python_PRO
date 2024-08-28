51044c1a728727ab73f6fe4110aff4f0fa28823d
Yonca <73234933+yoncasen@users.noreply.github.com>
2024-08-28 19:29:26 +0300

    Add files via upload

---
+++ M1L4/discord_bot.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands
+from sifre_olusturucu import sifre_olustur
+from bot_token import token
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} olarak giriş yaptık')
+
+@bot.command()
+async def merhaba(ctx):
+    await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def parola(ctx):
+  await ctx.send("Sizin için oluşturulan güvenli şifre:   " + sifre_olustur(10))
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+bot.run(token)
